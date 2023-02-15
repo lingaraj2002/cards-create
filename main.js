92971ed43b56009cf67efaf0d68615d1d82bfc6d
LINGARAJ <lingaraj01052002@gmail.com>
2023-02-15 17:09:03 +0530

    addingcrds.

---
+++ main.js
@@ -0,0 +1,285 @@
+
+let foods = [
+	
+	{
+		img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/uwr4quovqmry2umx4qkd',
+		restaurant:'Jayalakshmi Bakery & Sweets',
+		cusin:'Bakery, Sweets',
+		rate:'<a href="#"><i class="fa fa-star"> 3.9</i></a>',
+		time:'<a href="#">-- mins</a>',
+		price:'<a href="#">₹200 FOR TWO</a>',
+		offer:'<a href="#"><i class="fa fa-certificate" aria-hidden="true"> 50% off | Use WELCOME 50</i></a>',
+		ext:'<a href="#">QUICK VIEW</a>',
+	},
+	{
+		img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/f18ykkujtoocudo9bapd',
+		restaurant:'Rahumath Hotel',
+		cusin:'Indian',
+		rate:'<a href="#"><i class="fa fa-star"> 3.2</i></a>',
+		time:'<a href="#">-- mins</a>',
+		price:'<a href="#">₹200 FOR TWO</a>',
+		offer:'<a href="#"><i class="fa fa-certificate" aria-hidden="true"> 50% off | Use WELCOME 50</i></a>',
+		ext:'<a href="#">QUICK VIEW</a>',
+	},
+	{
+		img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/kfa4vxavextnftkauy3f',
+		restaurant:'Meat And Eat',
+		cusin:'Chinese, North Indian',
+		rate:'<a href="#"><i class="fa fa-star"> 4.3</i></a>',
+		time:'<a href="#">-- mins</a>',
+		price:'<a href="#">₹200 FOR TWO</a>',
+		offer:'<a href="#"><i class="fa fa-certificate" aria-hidden="true"> 50% off | Use WELCOME 50</i></a>',
+		ext:'<a href="#">QUICK VIEW</a>',
+	},
+	{
+		img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/qpouftkavlo6yiotfc5c',
+		restaurant:'B & P Restaurant',
+		cusin:'Italian, Beverages',
+		rate:'<a href="#"><i class="fa fa-star"> 3.5</i></a>',
+		time:'<a href="#">-- mins</a>',
+		price:'<a href="#">₹200 FOR TWO</a>',
+		offer:'<a href="#"><i class="fa fa-certificate" aria-hidden="true"> 50% off | Use WELCOME 50</i></a>',
+		ext:'<a href="#">QUICK VIEW</a>',
+	},
+	{
+		img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/vygixmrxr5oferlex6sc',
+		restaurant:'Krishna Tourist Home',
+		cusin:'North Indian, Chinese',
+		rate:'<a href="#"><i class="fa fa-star"> 3.3</i></a>',
+		time:'<a href="#">-- mins</a>',
+		price:'<a href="#">₹200 FOR TWO</a>',
+		offer:'<a href="#"><i class="fa fa-certificate" aria-hidden="true"> 50% off | Use WELCOME 50</i></a>',
+		ext:'<a href="#">QUICK VIEW</a>',
+	},
+	{
+		img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/zbgbwzvzz3qt5jdvmqdw',
+		restaurant:'Feast Pizza',
+		cusin:'Pizzas, Beverages',
+		rate:'<a href="#"><i class="fa fa-star"> 1.9</i></a>',
+		time:'<a href="#">-- mins</a>',
+		price:'<a href="#">₹200 FOR TWO</a>',
+		offer:'<a href="#"><i class="fa fa-certificate" aria-hidden="true"> 50% off | Use WELCOME 50</i></a>',
+		ext:'<a href="#">QUICK VIEW</a>',
+	},
+	{
+		img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/db2f7cf934d39bb022c7d7e97e769f33',
+		restaurant:'Hot N Chill',
+		cusin:'Indian',
+		rate:'<a href="#"><i class="fa fa-star"> 4.1</i></a>',
+		time:'<a href="#">-- mins</a>',
+		price:'<a href="#">₹200 FOR TWO</a>',
+		offer:'<a href="#"><i class="fa fa-certificate" aria-hidden="true"> 50% off | Use WELCOME 50</i></a>',
+		ext:'<a href="#">QUICK VIEW</a>',
+	},
+	{
+		img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/a5ptxdoppkgqgqnkci8w',
+		restaurant:'Abudhabi Biryani',
+		cusin:'Chinese, North Indian',
+		rate:'<a href="#"><i class="fa fa-star"> 3.0</i></a>',
+		time:'<a href="#">-- mins</a>',
+		price:'<a href="#">₹200 FOR TWO</a>',
+		offer:'<a href="#"><i class="fa fa-certificate" aria-hidden="true"> 50% off | Use WELCOME 50</i></a>',
+		ext:'<a href="#">QUICK VIEW</a>',
+	},
+	{
+		img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/uuzf6jcnitasoqccndnh',
+		restaurant:'Kwality Walls Frozen Dessert and Ice Cream Shop',
+		cusin:'Desserts, Ice Cream',
+		rate:'<a href="#"><i class="fa fa-star"> 3.9</i></a>',
+		time:'<a href="#">-- mins</a>',
+		price:'<a href="#">₹200 FOR TWO</a>',
+		offer:'<a href="#"><i class="fa fa-certificate" aria-hidden="true"> 50% off | Use WELCOME 50</i></a>',
+		ext:'<a href="#">QUICK VIEW</a>',
+	},
+	{
+		img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/xhbcrtzfy6cfx4sowncp',
+		restaurant:'Ibaco',
+		cusin:'Ice Cream, Desserts',
+		rate:'<a href="#"><i class="fa fa-star"> 4.5</i></a>',
+		time:'<a href="#">-- mins</a>',
+		price:'<a href="#">₹200 FOR TWO</a>',
+		offer:'<a href="#"><i class="fa fa-certificate" aria-hidden="true"> 50% off | Use WELCOME 50</i></a>',
+		ext:'<a href="#">QUICK VIEW</a>',
+	},
+	{
+		img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/yrvmawu458dgolexqeuw',
+		restaurant:'Greensland Restaurant',
+		cusin:'Chinese, North Indian',
+		rate:'<a href="#"><i class="fa fa-star"> 2.9</i></a>',
+		time:'<a href="#">-- mins</a>',
+		price:'<a href="#">₹200 FOR TWO</a>',
+		offer:'<a href="#"><i class="fa fa-certificate" aria-hidden="true"> 50% off | Use WELCOME 50</i></a>',
+		ext:'<a href="#">QUICK VIEW</a>',
+	},
+	{
+		img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/nrzmqzscpmbubxlkphyi',
+		restaurant:'Madurai Famous Jigarthanda Llp',
+		cusin:'Beverages',
+		rate:'<a href="#"><i class="fa fa-star"> 3.1</i></a>',
+		time:'<a href="#">-- mins</a>',
+		price:'<a href="#">₹200 FOR TWO</a>',
+		offer:'<a href="#"><i class="fa fa-certificate" aria-hidden="true"> 50% off | Use WELCOME 50</i></a>',
+		ext:'<a href="#">QUICK VIEW</a>',
+	},
+	{
+		img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/suhiraasnlj1tacub76v',
+		restaurant:'Chilly Restaurant',
+		cusin:'Chinese, North Indian',
+		rate:'<a href="#"><i class="fa fa-star"> 4.9</i></a>',
+		time:'<a href="#">-- mins</a>',
+		price:'<a href="#">₹200 FOR TWO</a>',
+		offer:'<a href="#"><i class="fa fa-certificate" aria-hidden="true"> 50% off | Use WELCOME 50</i></a>',
+		ext:'<a href="#">QUICK VIEW</a>',
+	},
+	{
+		img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/dtmmjfuikynzpg9pfbc1',
+		restaurant:'Fresho',
+		cusin:'Chinese, Burgers',
+		rate:'<a href="#"><i class="fa fa-star"> 4.1</i></a>',
+		time:'<a href="#">-- mins</a>',
+		price:'<a href="#">₹200 FOR TWO</a>',
+		offer:'<a href="#"><i class="fa fa-certificate" aria-hidden="true"> 50% off | Use WELCOME 50</i></a>',
+		ext:'<a href="#">QUICK VIEW</a>',
+	},
+	{
+		img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/wxudo6kr6u6cv3bzhi9x',
+		restaurant:'Topi Vappa Biriyani',
+		cusin:'Biryani, Chinese',
+		rate:'<a href="#"><i class="fa fa-star"> 5.0</i></a>',
+		time:'<a href="#">-- mins</a>',
+		price:'<a href="#">₹200 FOR TWO</a>',
+		offer:'<a href="#"><i class="fa fa-certificate" aria-hidden="true"> 50% off | Use WELCOME 50</i></a>',
+		ext:'<a href="#">QUICK VIEW</a>',
+	},
+	{
+		img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/pgnbsuaa1lhv8dc8glt7',
+		restaurant:'Chick Blast',
+		cusin:'Pastas, Chinese',
+		rate:'<a href="#"><i class="fa fa-star"> 3.2</i></a>',
+		time:'<a href="#">-- mins</a>',
+		price:'<a href="#">₹200 FOR TWO</a>',
+		offer:'<a href="#"><i class="fa fa-certificate" aria-hidden="true"> 50% off | Use WELCOME 50</i></a>',
+		ext:'<a href="#">QUICK VIEW</a>',
+	},
+	{
+		img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/jjanudisfcowk5l5jret',
+		restaurant:'American Bakery And Cafe',
+		cusin:'Chinese, Burgers',
+		rate:'<a href="#"><i class="fa fa-star"> 1.5</i></a>',
+		time:'<a href="#">-- mins</a>',
+		price:'<a href="#">₹200 FOR TWO</a>',
+		offer:'<a href="#"><i class="fa fa-certificate" aria-hidden="true"> 50% off | Use WELCOME 50</i></a>',
+		ext:'<a href="#">QUICK VIEW</a>',
+	},
+	{
+		img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/x5ejencwrp6qx9eyu9bh',
+		restaurant:'Aasife Biriyani',
+		cusin:'Desserts, Chinese',
+		rate:'<a href="#"><i class="fa fa-star"> 3.9</i></a>',
+		time:'<a href="#">-- mins</a>',
+		price:'<a href="#">₹200 FOR TWO</a>',
+		offer:'<a href="#"><i class="fa fa-certificate" aria-hidden="true"> 50% off | Use WELCOME 50</i></a>',
+		ext:'<a href="#">QUICK VIEW</a>',
+	},
+	{
+		img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/zvktyksfzrmiaz2bvrdh',
+		restaurant:'Nila Family Restaurant',
+		cusin:'Desserts, Chinese',
+		rate:'<a href="#"><i class="fa fa-star"> 2.9</i></a>',
+		time:'<a href="#">-- mins</a>',
+		price:'<a href="#">₹200 FOR TWO</a>',
+		offer:'<a href="#"><i class="fa fa-certificate" aria-hidden="true"> 50% off | Use WELCOME 50</i></a>',
+		ext:'<a href="#">QUICK VIEW</a>',
+	},
+	{
+		img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/qpouftkavlo6yiotfc5c',
+		restaurant:'B & P Restaurant',
+		cusin:'Italian, Beverages',
+		rate:'<a href="#"><i class="fa fa-star"> 2.9</i></a>',
+		time:'<a href="#">-- mins</a>',
+		price:'<a href="#">₹200 FOR TWO</a>',
+		offer:'<a href="#"><i class="fa fa-certificate" aria-hidden="true"> 50% off | Use WELCOME 50</i></a>',
+		ext:'<a href="#">QUICK VIEW</a>',
+	},
+	
+	
+	]
+	
+	
+
+
+
+
+let section = document.createElement("section");
+document.body.appendChild(section);
+
+let container = document.createElement("div");
+container.classList.add("container");
+section.appendChild(container);
+
+let row = document.createElement("div");
+row.classList.add("row");
+container.appendChild(row);
+
+foods.forEach(function(le){
+
+let col = document.createElement("div");
+col.classList.add("col");
+row.appendChild(col);
+
+let card = document.createElement("div");
+card.classList.add("card");
+col.appendChild(card);
+
+let img = document.createElement("img");
+img.src = le.img;
+card.appendChild(img);
+
+let heading1 = document.createElement("h3");
+heading1.innerHTML = le.restaurant;
+card.appendChild(heading1);
+
+let heading2 = document.createElement("h4");
+heading2.innerHTML = le.cusin;
+card.appendChild(heading2);
+
+let rating = document.createElement("div");
+rating.classList.add("row2");
+card.appendChild(rating);
+
+let rate1 = document.createElement("div");
+rate1.classList.add("rate1");
+rate1.innerHTML = le.rate;
+rating.appendChild(rate1);
+
+let rate2 = document.createElement("div");
+rate2.classList.add("rate2");
+rate2.innerHTML = le.time;
+rating.appendChild(rate2);
+
+let rate3 = document.createElement("div");
+rate3.classList.add("rate3");
+rate3.innerHTML = le.price;
+rating.appendChild(rate3);
+
+let dis = document.createElement("div");
+dis.classList.add("dis");
+card.appendChild(dis);
+
+let heading3 = document.createElement("h5");
+heading3.innerHTML = le.offer;
+dis.appendChild(heading3);
+
+let view = document.createElement("div");
+view.classList.add("view");
+card.appendChild(view);
+
+let heading4 = document.createElement("h4");
+heading4.innerHTML = le.ext;
+view.appendChild(heading4);
+
+})
+
+
+
+
